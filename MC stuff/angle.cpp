630350ab8a92e005f9e9d5d883dc8c4efeaa5254
DarkInViolet <arobertdicu@gmail.com>
2021-08-06 17:29:41 +0300

    Da-i run sa vezi

--- MC stuff/angle.cpp
+++ MC stuff/angle.cpp
@@ -4,3 +4,12 @@
 Angle::Angle() : v(0) { }
 Angle::Angle(byte value) : v(value) { }
 Angle::Angle(double value) : v(fmod(value, 360) / 360 * 256) { }
+
+Angle::operator double()
+{
+	return v * 1.40625;
+}
+void Angle::operator=(double value)
+{
+	v = (fmod(value, 360) / 360 * 256);
+}
