0a4cfad36e4e0c7c932ae974e467c147b5a5b39e
Lazar Petru <adrian_petru72@yahoo.com>
2021-09-28 17:11:38 +0300

    block.* files

---
+++ MC stuff/lib/ServerApi/world/block.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "../types/apidef.h"
+#include "../types/varData.h"
+
+class PaletteEntry
+{
+public:
+	short referenceCount;
+	class Block
+	{
+	public:
+		varInt id;
+
+
+	} block;
+};
