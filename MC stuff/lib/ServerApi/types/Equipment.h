1e62e8c4c051cc90f4ae615ecdc268b86b7cb658
CosminPerRam <cosmin.p@live.com>
2021-10-03 23:48:55 +0300

    Correct implementation of Entity Equipment
    Si acum nu mai da crash, dupa 10 ani

--- MC stuff/lib/ServerApi/types/Equipment.h
+++ MC stuff/lib/ServerApi/types/Equipment.h
@@ -3,18 +3,19 @@
 #include "slot.h"
 #include "item.h"
 
+#include <iostream>
+
 class Equipment {
 private:
 	Byte slot;
 	Slot* item;
 
 public:
-	Equipment(Byte slot, Slot*& item) : slot(slot), item(item) {
+	Equipment(Byte slot, Slot* item) : slot(slot), item(item) {
 
 	}
 
 	void write(char*& buffer) const;
 
 	Byte getSlot() { return slot; }
-	Slot* getItem() { return item; }
 };
