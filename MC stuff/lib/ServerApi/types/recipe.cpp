339325e82b40235e1ac1e9f35f762478c95af06f
CosminPerRam <cosmin.p@live.com>
2021-11-22 16:58:26 +0200

    Loading recipes done

--- MC stuff/lib/ServerApi/types/recipe.cpp
+++ MC stuff/lib/ServerApi/types/recipe.cpp
@@ -7,6 +7,11 @@
 
 namespace recipe
 {
+	const std::string Recipe::crafting_shapeless = "minecraft:crafting_shapeless", Recipe::crafting_shaped = "minecraft:crafting_shaped", Recipe::smelting = "minecraft:smelting", Recipe::blasting = "minecraft:blasting", Recipe::smoking = "minecraft:smoking", Recipe::campfire_cooking = "minecraft:campfire_cooking", Recipe::stonecutting = "minecraft:stonecutting", Recipe::smithing = "minecraft:smithing",
+		Recipe::crafting_special_armordye = "minecraft:crafting_special_armordye", Recipe::crafting_special_bookcloning = "minecraft:crafting_special_bookcloning", Recipe::crafting_special_mapcloning = "minecraft:crafting_special_mapcloning", Recipe::crafting_special_mapextending = "minecraft:crafting_special_mapextending", Recipe::crafting_special_firework_rocket = "minecraft:crafting_special_firework_rocket",
+		Recipe::crafting_special_firework_star = "minecraft:crafting_special_firework_star", Recipe::crafting_special_firework_star_fade = "minecraft:crafting_special_firework_star_fade", Recipe::crafting_special_repairitem = "minecraft:crafting_special_repairitem", Recipe::crafting_special_tippedarrow = "minecraft:crafting_special_tippedarrow", Recipe::crafting_special_bannerduplicate = "minecraft:crafting_special_bannerduplicate",
+		Recipe::crafting_special_banneraddpattern = "minecraft:crafting_special_banneraddpattern", Recipe::crafting_special_shielddecoration = "minecraft:crafting_special_shielddecoration", Recipe::crafting_special_shulkerboxcoloring = "minecraft:crafting_special_shulkerboxcoloring", Recipe::crafting_special_suspiciousstew = "minecraft:crafting_special_suspiciousstew";
+
 	std::vector<Recipe*>* Manager::recipes;
 	std::vector<mcString>* Manager::recipesIDs;
 
@@ -22,13 +27,50 @@ namespace recipe
 		type.write(buffer);
 		recipeID.write(buffer);
 	}
+
+	Ingredient* Manager::getItemOrTag(json* data)
+	{
+		Slot** slotArray = new Slot * [1];
+		ull slotArraySize = 1;
+
+		if (data->getType() == json::type::array) {
+			delete[] slotArray;
+			slotArraySize = data->getSize();
+			slotArray = new Slot * [slotArraySize];
+
+			for (int j = 0; j < slotArraySize; j++) //array obj can be only item?
+				slotArray[j] = new Slot(true, Registry::getId(Registry::itemRegistry, (*data)[j]["item"].value()), 1);
+			return new Ingredient(slotArraySize, slotArray);
+		}
+		//else, compound
+
+		if (data->has("item"))
+			slotArray[0] = new Slot(true, Registry::getId(Registry::itemRegistry, (*data)["item"].value()), 1);
+		else { //tag
+			std::string tagName = (*data)["tag"].value();
+
+			const TagGroup::Tag* theTag = TagGroup::getTag(TagGroup::itemTags, tagName); //search it
+
+			if (theTag == nullptr)
+				throw "tag not found, recipe couldn't be parsed";
+
+			delete[] slotArray;
+			slotArraySize = theTag->entries.size();
+			slotArray = new Slot * [slotArraySize];
+			for (int j = 0; j < slotArraySize; j++)
+				slotArray[j] = new Slot(true, theTag->entries[j], 1);
+		}
+
+		return new Ingredient(slotArraySize, slotArray);
+	}
+
 	static unsigned recipeID = 0;
 	void Manager::loadRecipes()
 	{
 		recipes = new std::vector<Recipe*>();
 		recipesIDs = new std::vector<mcString>();
 
-		std::filesystem::directory_iterator recipesDir("data/recipes/others");
+		std::filesystem::directory_iterator recipesDir("data/recipes");
 
 		for (auto const& recipeFile : recipesDir)
 		{
@@ -41,81 +83,106 @@ namespace recipe
 			recipe::Recipe* theRecipe;
 			std::string type = (*data)["type"].value();
 
-			if (type == "minecraft:crafting_shapeless") {
-				std::cout <<"\n"<< recipeFile.path() << " ";
-
+			if (type == Recipe::crafting_shapeless) {
 				mcString group = recipeFile.path().stem().string();
 				if (data->has("group"))
-				{
-					std::cout << "G ";
 					group = (*data)["group"].value();
-				}
-
-				varInt ingredientCount = (*data)["ingredients"].getSize();
-				Ingredient** ingredients = new Ingredient * [ingredientCount];
-				for (int i = 0; i < ingredientCount; i++) {
-					if ((*data)["ingredients"][i].getType() == json::type::array) {
-						json* theArray = &(*data)["ingredients"][i];
-
-						Slot** slotArray = new Slot * [theArray->getSize()];
-						for(int j = 0; j < theArray->getSize(); j++)
-							slotArray[j] = new Slot(true, Registry::getId(Registry::itemRegistry, (*data)["ingredients"][i][j]["item"].value()), 1);
-						ingredients[i] = new Ingredient(theArray->getSize(), slotArray);
-					} 
-					else { //compound
-						Slot** slotArray = new Slot * [1];
-						int slotArraySize = 1;
-
-						if ((*data)["ingredients"][i].has("item"))
-							slotArray[0] = new Slot(true, Registry::getId(Registry::itemRegistry, (*data)["ingredients"][i]["item"].value()), 1);
-						else { //tag
-							delete[] slotArray;
-
-							std::string tagName = (*data)["ingredients"][i]["tag"].value();
-							std::cout << "\n" << tagName;
-
-							const TagGroup::Tag* theTag = nullptr; //search it
-
-							if (theTag == nullptr)
-								throw "tag not found, recipe couldn't be parsed";
-
-
-							/*for (const TagGroup& t : TagGroup::defaultTags) {
-								for (const TagGroup::Tag& at : t.tags) {
-									if(at.name == tagName)
-										std::cout << "\n" << at.name;
-								}
-							}*/
-						}
-
-						ingredients[i] = new Ingredient(slotArraySize, slotArray);
-					}
-
-					
-
-					/*if ((*data)["ingredients"][i].exists("item"))
-						std::cout << "i: "<<(*data)["ingredients"][i]["item"].value() << "\n";
-					else if ((*data)["ingredients"][i].exists("tag"))
-						std::cout << "t: " << (*data)["ingredients"][i]["tag"].value() << "\n";*/
-				}
-
 				varInt resultCount = 1;
 				if ((*data)["result"].has("count"))
 					resultCount = (*data)["result"]["count"].iValue();
 				Slot* result = new Slot(true, Registry::getId(Registry::itemRegistry, (*data)["result"]["item"].value()), resultCount);
 
+				varInt ingredientCount = (*data)["ingredients"].getSize();
+				Ingredient** ingredients = new Ingredient * [ingredientCount];
+				for (int i = 0; i < ingredientCount; i++)
+					ingredients[i] = getItemOrTag(&(*data)["ingredients"][i]);
+
 				theRecipe = new recipe::crafting_shapeless(recipeFile.path().stem().string(), group, ingredientCount, ingredients, result);
-				recipesIDs->emplace_back(recipeFile.path().stem().string());
-			} else if (type == "minecraft:crafting_shaped") {
+			} else if (type == Recipe::crafting_shaped) {
+				mcString group = recipeFile.path().stem().string();
+				if (data->has("group"))
+					group = (*data)["group"].value();
+				varInt resultCount = 1;
+				if ((*data)["result"].has("count"))
+					resultCount = (*data)["result"]["count"].iValue();
+				Slot* result = new Slot(true, Registry::getId(Registry::itemRegistry, (*data)["result"]["item"].value()), resultCount);
 
-			} else if (type == "minecraft:smelting" || type == "minecraft:blasting" || type == "minecraft:smoking" || type == "minecraft:campfire_cooking") {
+				int height = (*data)["pattern"].getSize();
+				int width = 1;
+				for (int i = 0; i < height; i++)
+				{
+					int rowSize = (*data)["pattern"][i].value().size();
+					if ((*data)["pattern"][i].value().find('\\') != std::string::npos)
+						rowSize = rowSize - 5;
+					
+					if (rowSize > width)
+						width = rowSize;
+				}
 
-			} else if (type == "minecraft:stonecutting") {
+				Ingredient** ingredients = new Ingredient * [width * height];
+				for (int x = 0; x < height; x++) {
+					std::string row = (*data)["pattern"][x].value();
+					int accessY = 0;
+					for (int y = 0; y < width; y++) {
+						if (row[accessY] == ' ') {
+							Slot** slotArray = new Slot * [1];
+							slotArray[0] = new Slot(false, 0);
+							ingredients[y + x * width] = new Ingredient(1, slotArray);
+						}
+						else if (row[accessY] == '\\') {
+							ingredients[y + x * width] = getItemOrTag(&(*data)["key"][row.substr(accessY, 6)]);
+							accessY = accessY + 5;
+						}
+						else
+							ingredients[y + x * width] = getItemOrTag(&(*data)["key"][std::string(1, row[accessY])]);
 
-			} else if (type == "minecraft:smithing") {
+						accessY += 1;
+					}
+				}
 
+				theRecipe = new recipe::crafting_shaped(recipeFile.path().stem().string(), width, height, group, ingredients, result);
+			} else if (type == Recipe::smelting || type == Recipe::blasting || type == Recipe::smoking || type == Recipe::campfire_cooking) {
+				mcString group = recipeFile.path().stem().string();
+				if (data->has("group"))
+					group = (*data)["group"].value();
+				Slot* result = new Slot(true, Registry::getId(Registry::itemRegistry, (*data)["result"].value()), 1); //maximum of 1 item?
+				bfloat experience = (*data)["experience"].dValue();
+				varInt cookingTime = (*data)["cookingtime"].iValue();
+
+				Ingredient* ingredient = getItemOrTag(&(*data)["ingredient"]);
+
+				if(type == Recipe::smelting)
+					theRecipe = new recipe::smelting(recipeFile.path().stem().string(), group, ingredient, result, experience, cookingTime);
+				else if (type == Recipe::blasting)
+					theRecipe = new recipe::blasting(recipeFile.path().stem().string(), group, ingredient, result, experience, cookingTime);
+				else if (type == Recipe::smoking)
+					theRecipe = new recipe::smoking(recipeFile.path().stem().string(), group, ingredient, result, experience, cookingTime);
+				else //campfire_cooking
+					theRecipe = new recipe::campfire_cooking(recipeFile.path().stem().string(), group, ingredient, result, experience, cookingTime);
+			} else if (type == Recipe::stonecutting) {
+				mcString group = recipeFile.path().stem().string();
+				if (data->has("group"))
+					group = (*data)["group"].value();
+				varInt resultCount = 1;
+				if ((*data).has("count"))
+					resultCount = (*data)["count"].iValue();
+				Slot* result = new Slot(true, Registry::getId(Registry::itemRegistry, (*data)["result"].value()), resultCount);
+
+				Ingredient* ingredient = getItemOrTag(&(*data)["ingredient"]);
+
+				theRecipe = new recipe::stonecutting(recipeFile.path().stem().string(), group, ingredient, result);
+			} else if (type == Recipe::smithing) {
+				Ingredient* base = getItemOrTag(&(*data)["base"]);
+				Ingredient* addition = getItemOrTag(&(*data)["addition"]);
+
+				Slot* result = new Slot(true, Registry::getId(Registry::itemRegistry, (*data)["result"]["item"].value()), 1);
+
+				theRecipe = new recipe::smithing(recipeFile.path().stem().string(), base, addition, result);
 			}
+			else //other recipies that dont have any extra data
+				theRecipe = new recipe::Recipe(type, recipeFile.path().stem().string());
 
+			recipesIDs->emplace_back(recipeFile.path().stem().string());
 			recipes->emplace_back(theRecipe);
 		}
 	}
--- MC stuff/lib/ServerApi/types/recipe.h
+++ MC stuff/lib/ServerApi/types/recipe.h
@@ -30,11 +30,19 @@ namespace recipe
 	public:
 		Recipe(mcString type, mcString recipeID) : type(type), recipeID(recipeID) {}
 
+		static const std::string crafting_shapeless, crafting_shaped, smelting, blasting, smoking, campfire_cooking, stonecutting, smithing,
+			crafting_special_armordye, crafting_special_bookcloning, crafting_special_mapcloning, crafting_special_mapextending, crafting_special_firework_rocket,
+			crafting_special_firework_star, crafting_special_firework_star_fade, crafting_special_repairitem, crafting_special_tippedarrow, crafting_special_bannerduplicate,
+			crafting_special_banneraddpattern, crafting_special_shielddecoration, crafting_special_shulkerboxcoloring, crafting_special_suspiciousstew;
+		
 		virtual void write(char*& buffer) const;
 	};
 
 	class Manager
 	{
+	private:
+		static Ingredient* getItemOrTag(json* data);
+
 	public:
 		static std::vector<Recipe*>* recipes;
 		static std::vector<mcString>* recipesIDs;
@@ -57,7 +65,7 @@ namespace recipe
 		void write(char*& buffer) const;
 	};
 
-	class crafting_shaped : Recipe
+	class crafting_shaped : public Recipe
 	{
 	public:
 		varInt width, height;
@@ -71,91 +79,91 @@ namespace recipe
 		void write(char*& buffer) const;
 	};
 
-	struct crafting_special_armordye : Recipe
+	struct crafting_special_armordye : public Recipe
 	{
 		crafting_special_armordye(mcString recipeID) 
 			: Recipe("crafting_special_armordye", recipeID) {}
 	};
 
-	struct crafting_special_bookcloning : Recipe
+	struct crafting_special_bookcloning : public Recipe
 	{
 		crafting_special_bookcloning(mcString recipeID) 
 			: Recipe("crafting_special_bookcloning", recipeID) {}
 	};
 
-	struct crafting_special_mapcloning : Recipe
+	struct crafting_special_mapcloning : public Recipe
 	{
 		crafting_special_mapcloning(mcString recipeID) 
 			: Recipe("crafting_special_mapcloning", recipeID) {}
 	};
 
-	struct crafting_special_mapextending : Recipe
+	struct crafting_special_mapextending : public Recipe
 	{
 		crafting_special_mapextending(mcString recipeID) 
 			: Recipe("crafting_special_mapextending", recipeID) {}
 	};
 
-	struct crafting_special_firework_rocket : Recipe
+	struct crafting_special_firework_rocket : public Recipe
 	{
 		crafting_special_firework_rocket(mcString recipeID) 
 			: Recipe("crafting_special_firework_rocket", recipeID) {}
 	};
 
-	struct crafting_special_firework_star : Recipe
+	struct crafting_special_firework_star : public Recipe
 	{
 		crafting_special_firework_star(mcString recipeID) 
 			: Recipe("crafting_special_firework_star", recipeID) {}
 	};
 
-	struct crafting_special_firework_star_fade : Recipe
+	struct crafting_special_firework_star_fade : public Recipe
 	{
 		crafting_special_firework_star_fade(mcString recipeID) 
 			: Recipe("crafting_special_firework_star_fade", recipeID) {}
 	};
 
-	struct crafting_special_repairitem : Recipe
+	struct crafting_special_repairitem : public Recipe
 	{
 		crafting_special_repairitem(mcString recipeID) 
 			: Recipe("crafting_special_repairitem", recipeID) {}
 	};
 
-	struct crafting_special_tippedarrow : Recipe
+	struct crafting_special_tippedarrow : public Recipe
 	{
 		crafting_special_tippedarrow(mcString recipeID) 
 			: Recipe("crafting_special_tippedarrow", recipeID) {}
 	};
 
-	struct crafting_special_bannerduplicate : Recipe
+	struct crafting_special_bannerduplicate : public Recipe
 	{
 		crafting_special_bannerduplicate(mcString recipeID) 
 			: Recipe("crafting_special_bannerduplicate", recipeID) {}
 	};
 
-	struct crafting_special_banneraddpattern : Recipe
+	struct crafting_special_banneraddpattern : public Recipe
 	{
 		crafting_special_banneraddpattern(mcString recipeID) 
 			: Recipe("crafting_special_banneraddpattern", recipeID) {}
 	};
 
-	struct crafting_special_shielddecoration : Recipe
+	struct crafting_special_shielddecoration : public Recipe
 	{
 		crafting_special_shielddecoration(mcString recipeID) 
 			: Recipe("crafting_special_shielddecoration", recipeID) {}
 	};
 
-	struct crafting_special_shulkerboxcoloring : Recipe
+	struct crafting_special_shulkerboxcoloring : public Recipe
 	{
 		crafting_special_shulkerboxcoloring(mcString recipeID) 
 			: Recipe("crafting_special_shulkerboxcoloring", recipeID) {}
 	};
 
-	struct crafting_special_suspiciousstew : Recipe
+	struct crafting_special_suspiciousstew : public Recipe
 	{
 		crafting_special_suspiciousstew(mcString recipeID) 
 			: Recipe("crafting_special_suspiciousstew", recipeID) {}
 	};
 
-	class smelting : Recipe
+	class smelting : public Recipe
 	{
 	public:
 		mcString group;
@@ -170,7 +178,7 @@ namespace recipe
 		void write(char*& buffer) const;
 	};
 
-	class blasting : Recipe
+	class blasting : public Recipe
 	{
 	public:
 		mcString group;
@@ -185,7 +193,7 @@ namespace recipe
 		void write(char*& buffer) const;
 	};
 
-	class smoking : Recipe
+	class smoking : public Recipe
 	{
 	public:
 		mcString group;
@@ -200,7 +208,7 @@ namespace recipe
 		void write(char*& buffer) const;
 	};
 
-	class campfire_cooking : Recipe
+	class campfire_cooking : public Recipe
 	{
 	public:
 		mcString group;
@@ -215,7 +223,7 @@ namespace recipe
 		void write(char*& buffer) const;
 	};
 
-	class stonecutting : Recipe
+	class stonecutting : public Recipe
 	{
 	public:
 		mcString group;
@@ -228,7 +236,7 @@ namespace recipe
 		void write(char*& buffer) const;
 	};
 
-	class smithing : Recipe
+	class smithing : public Recipe
 	{
 	public:
 		Ingredient* base, * addition;
