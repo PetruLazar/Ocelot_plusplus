a0e2a4814346e032ce184610f2fb69144a1a80cd
Lazar Petru <adrian_petru72@yahoo.com>
2021-11-26 14:58:41 +0200

    created a solution directory for custom generators and changed a bit the project template for custom generators

--- MC stuff/generators/CustomWGenTest/generator.cpp
+++ MC stuff/generators/CustomWGenTest/generator.cpp
@@ -8,13 +8,16 @@ Chunk* generate(World* wld, int chunkX, int chunkZ)
 
 	ull sectionCount = wldHeight >> 4;
 
-	//light section below ground - completly dark
+	//light section below ground - empty
 	{
+		//empty light mask arrays seem to be ignored - all sections that are marked absent in the mask are considered empty
+		//empty section probably means full 15 sky light and full 0 block light
+		
 		//mark light section as empty
 		ch->emptyBlockLightMask->setElement(0, 1);
 		ch->emptySkyLightMask->setElement(0, 1);
 	}
-	//light section above ground - completly dark
+	//light section above ground - empty
 	{
 		//mark light section as empty
 		ch->emptyBlockLightMask->setElement(sectionCount, 1);
@@ -35,15 +38,13 @@ Chunk* generate(World* wld, int chunkX, int chunkZ)
 		section.blockCount = 0;
 		section.bitsPerBlock = 4;
 		section.useGlobalPallete = false;
-		section.palette.push_back(PaletteEntry(BlockState("minecraft:air"), 0x1000));
+		section.palette.emplace_back(PaletteEntry(BlockState("minecraft:air"), 0x1000));
 		section.blockStates = new BitArray(4096, section.bitsPerBlock);
 
 		//biome data
 		for (ull i = 0; i < sizeof(section.biomes) / sizeof(ull); i++) ((ull*)section.biomes)[i] = 0;
 
-		//LIGHTSECTION DATA - this is a completly dark section
-		//lightSection.skyLight = new BitArray(4096, 4);
-		//lightSection.blockLight = new BitArray(4096, 4);
+		//LIGHTSECTION DATA - this section is light empty
 
 		//mark light sections as empty
 		ch->emptyBlockLightMask->setElement(i + 1, 1);
