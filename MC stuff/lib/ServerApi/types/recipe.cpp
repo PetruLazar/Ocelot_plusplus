e6511408f9d1fff33707582b0784998aa186c8b9
CosminPerRam <cosmin.p@live.com>
2022-01-29 20:00:13 +0200

    Recipes divided load

--- MC stuff/lib/ServerApi/types/recipe.cpp
+++ MC stuff/lib/ServerApi/types/recipe.cpp
@@ -64,22 +64,21 @@ namespace recipe
 		return new Ingredient(slotArraySize, slotArray);
 	}
 
-	const Recipe* Manager::getRecipe(const std::string& recipeID)
+	void Manager::addRecipe(const mcString& id, recipe::Recipe* recipe)
 	{
-		for (Recipe* recipe : *recipes)
-		{
-			if (recipe->getRecipeID() == recipeID)
-				return recipe;
-		}
-
-		return nullptr;
+		recipesIDs->emplace_back(id);
+		recipes->emplace_back(recipe);
 	}
 
-	void Manager::loadRecipes()
+	void Manager::loadStaticRecipes()
 	{
-		recipes = new std::vector<Recipe*>();
-		recipesIDs = new std::vector<mcString>();
+		//std::cout << "acacia planks: " << Registry::getId(Registry::itemRegistry, "minecraft:acacia_planks") << std::endl; //26
+		//std::cout << "acacia boat: " << Registry::getId(Registry::itemRegistry, "minecraft:acacia_boat") << std::endl; //674
+		//Manager::addRecipe("acacia_boat", new recipe::crafting_shaped("acacia_boat", 3, 2, "boat", new Ingredient * [3 * 2]{ new Slot(true, 674, 1) }, new Slot(true, 674, 1)));
+	}
 
+	void Manager::loadDynamicRecipes()
+	{
 		std::filesystem::directory_iterator recipesDir("data/recipes");
 
 		for (auto const& recipeFile : recipesDir)
@@ -108,7 +107,8 @@ namespace recipe
 					ingredients[i] = getItemOrTag(&(*data)["ingredients"][i]);
 
 				theRecipe = new recipe::crafting_shapeless(recipeFile.path().stem().string(), group, ingredientCount, ingredients, result);
-			} else if (type == Recipe::crafting_shaped) {
+			}
+			else if (type == Recipe::crafting_shaped) {
 				mcString group = recipeFile.path().stem().string();
 				if (data->has("group"))
 					group = (*data)["group"].value();
@@ -124,7 +124,7 @@ namespace recipe
 					int rowSize = (*data)["pattern"][i].value().size();
 					if ((*data)["pattern"][i].value().find('\\') != std::string::npos)
 						rowSize = rowSize - 5;
-					
+
 					if (rowSize > width)
 						width = rowSize;
 				}
@@ -151,7 +151,8 @@ namespace recipe
 				}
 
 				theRecipe = new recipe::crafting_shaped(recipeFile.path().stem().string(), width, height, group, ingredients, result);
-			} else if (type == Recipe::smelting || type == Recipe::blasting || type == Recipe::smoking || type == Recipe::campfire_cooking) {
+			}
+			else if (type == Recipe::smelting || type == Recipe::blasting || type == Recipe::smoking || type == Recipe::campfire_cooking) {
 				mcString group = recipeFile.path().stem().string();
 				if (data->has("group"))
 					group = (*data)["group"].value();
@@ -161,7 +162,7 @@ namespace recipe
 
 				Ingredient* ingredient = getItemOrTag(&(*data)["ingredient"]);
 
-				if(type == Recipe::smelting)
+				if (type == Recipe::smelting)
 					theRecipe = new recipe::smelting(recipeFile.path().stem().string(), group, ingredient, result, experience, cookingTime);
 				else if (type == Recipe::blasting)
 					theRecipe = new recipe::blasting(recipeFile.path().stem().string(), group, ingredient, result, experience, cookingTime);
@@ -169,7 +170,8 @@ namespace recipe
 					theRecipe = new recipe::smoking(recipeFile.path().stem().string(), group, ingredient, result, experience, cookingTime);
 				else //campfire_cooking
 					theRecipe = new recipe::campfire_cooking(recipeFile.path().stem().string(), group, ingredient, result, experience, cookingTime);
-			} else if (type == Recipe::stonecutting) {
+			}
+			else if (type == Recipe::stonecutting) {
 				mcString group = recipeFile.path().stem().string();
 				if (data->has("group"))
 					group = (*data)["group"].value();
@@ -181,7 +183,8 @@ namespace recipe
 				Ingredient* ingredient = getItemOrTag(&(*data)["ingredient"]);
 
 				theRecipe = new recipe::stonecutting(recipeFile.path().stem().string(), group, ingredient, result);
-			} else if (type == Recipe::smithing) {
+			}
+			else if (type == Recipe::smithing) {
 				Ingredient* base = getItemOrTag(&(*data)["base"]);
 				Ingredient* addition = getItemOrTag(&(*data)["addition"]);
 
@@ -192,9 +195,28 @@ namespace recipe
 			else //other recipies that dont have any extra data
 				theRecipe = new recipe::Recipe(type, recipeFile.path().stem().string());
 
-			recipesIDs->emplace_back(recipeFile.path().stem().string());
-			recipes->emplace_back(theRecipe);
+			Manager::addRecipe(recipeFile.path().stem().string(), theRecipe);
 		}
+	}
+
+	const Recipe* Manager::getRecipe(const std::string& recipeID)
+	{
+		for (Recipe* recipe : *recipes)
+		{
+			if (recipe->getRecipeID() == recipeID)
+				return recipe;
+		}
+
+		return nullptr;
+	}
+
+	void Manager::loadRecipes()
+	{
+		recipes = new std::vector<Recipe*>();
+		recipesIDs = new std::vector<mcString>();
+
+		loadStaticRecipes();
+		loadDynamicRecipes();
 	}
 
 	void Manager::unloadRecipes()
