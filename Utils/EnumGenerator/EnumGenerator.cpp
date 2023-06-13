918825b9f728d64d2128b99ca5107c5b9ba76514
Lazar Petru <adrian_petru72@yahoo.com>
2023-06-13 17:17:45 +0300

    enum generator changes

--- Utils/EnumGenerator/EnumGenerator.cpp
+++ Utils/EnumGenerator/EnumGenerator.cpp
@@ -33,12 +33,48 @@ int main(int argc, char** args)
 	json& registries = *json::parse(registriesFile);
 
 	//open the output file
-	ofstream out(argc == 3 ? args[2] : "registry_enums");
+	ofstream out(argc == 3 ? args[2] : "registry_enums.txt");
 	if (!out.is_open())
 	{
 		cout << "Cannot open the output file.";
 		return 3;
 	}
+
+	//search for properties
+	vector<json*> list;
+	//take each block in the json
+	ull size = registries.getSize();
+	for (ull i = 0; i < size; i++)
+	{
+		try
+		{
+			json& properties = registries[i]["properties"];
+			ull size = properties.getSize();
+			for (ull j = 0; j < size; j++)
+			{
+				json& prop = properties[j];
+				bool already = false;
+				for (json* entry : list)
+				{
+					if (entry->getName() == prop.getName() && entry->getSize() == prop.getSize())
+					{
+						already = true;
+						break;
+					}
+				}
+				if (already) continue;
+				list.emplace_back(&prop);
+			}
+		}
+		catch (const char[]) {}
+	}
+
+	for (json* prop : list) out << prop->to_string() << '\n';
+
+	delete &registries;
+	registriesFile.close();
+	out.close();
+	return 0;
 
 	//produce output
 	//start the Item enum
@@ -69,5 +105,6 @@ int main(int argc, char** args)
 	//clean up
 	delete &registries;
 	registriesFile.close();
+	out.close();
 	return 0;
 }
